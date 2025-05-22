07babaa79d89261bd51ee104db97321bef0b4877
5b1g0030 <5b1g0030@stust.edu.tw>
2025-05-22 10:51:16 +0800

    網頁路由問題(已解決)

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+function About() {
+    return (
+      <div>
+        <h1>關於我</h1>
+        <p>這是關於這個網站的介紹。</p>
+      </div>
+    );
+  }
+  export default About;
+  
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+      <div>
+        <h1 style={{ color: 'blue' }}>這是首頁！歡迎光臨！</h1>
+
+      </div>
+    );
+  }
+  export default Home;
+  
